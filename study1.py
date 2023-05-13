efd1c26e8d7373895f1883601d720b844592c36e
nadav daniel <nadav.daniel@gmail.com>
2023-05-13 10:40:59 +0300

    as

---
+++ study1.py
@@ -0,0 +1,12 @@
+calc_to_unit= 24
+name_of_unit = "hours"
+def days_of_units(num_of_days):
+    print(f"{num_of_days} days are {num_of_days * calc_to_unit}")
+    # print(f"{num_of_days} {20 * to_second} {name_of_unit}")
+    # print(f"34 days are {35 * to_second} minutes")
+days_of_units(35)
+days_of_units(40)
+days_of_units(50)
+#The days of units (35) define the number for num_of_days)
+
+
